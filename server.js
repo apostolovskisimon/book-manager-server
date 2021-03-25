0875f400225d643887a60a9577674a73aa73e58b
simonapostolovski <simon.apostolovski@atis.al>
2021-03-25 14:28:20 +0100

    asdsd

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("yooo");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
